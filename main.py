38d4fcc0dc1c08f2fea5eb63319c80d29b406210
avtk <a.viticchie@awaves.it>
2023-03-21 12:06:27 +0100

    Second

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 lattine: str = 9
 lattine = 10
 lattine = 'lattine scritto'
@@ -12,5 +13,19 @@ c = float(0x7000)
 cf = int(67.0)
 cs = '1234567asd'
 
-
 print(c == cf)
+
+cameCase = 9
+
+COSTANTE = 3.14
+print(COSTANTE)
+
+COSTANTE = 2.38
+
+print(7 // 4)
+print(7 % 4)
+
+max(4, 5)
+
+
+sqrt(2)
---
+++ main_import.py
@@ -0,0 +1,7 @@
+import math as mt
+import os
+
+
+v = 4
+
+print(mt.sqrt(2) * mt.pi)
