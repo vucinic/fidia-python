f4c0d0324cbd5d1ae242621a2b2d74bd10e5b03e
avtk <a.viticchie@awaves.it>
2023-03-28 13:05:04 +0200

    28 03 esercizio immatricolazioni

---
+++ 03_28_exe_immatricolazioni.py
@@ -0,0 +1,21 @@
+m1 = input('Matricola 1: ')
+m2 = input('Matricola 2: ')
+
+valid = True
+
+# region Checks...
+if not m1[1:].isdigit():
+    print(f'Matricola 1 non valida: {m1}')
+    valid = False
+
+if not m2[1:].isdigit():
+    print(f'Matricola 2 non valida: {m2}')
+    valid = False
+# endregion
+
+if valid:
+
+    if int(m1[1:]) > int(m2[1:]):
+        print(m2, m1)
+    else:
+        print(m1, m2)
