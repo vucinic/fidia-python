7731cdcf77bdd4eb383b5d318ba7c970737e54bb
avtk <a.viticchie@awaves.it>
2023-03-28 12:21:00 +0200

    28 03

---
+++ 03_28_main.py
@@ -0,0 +1,17 @@
+dato = input('Dato: ')
+
+if 'fuck' not in dato:
+    print("Ok bravo")
+
+# resoconto.docx
+# resoconto.DOCX
+
+if dato.lower().endswith('.docx'):
+    print('e` word')
+
+
+intero = 213
+if (intero > 10):
+    print()
+
+print(dato)
